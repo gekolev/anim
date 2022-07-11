9e757ff508072c11a132873d10f82c49dc36ff45
George Kolev <gogo1520180@gmail.com>
2022-07-11 13:27:48 +0300

    yaaa

--- first/header.js
+++ first/header.js
@@ -6,10 +6,20 @@ fadeInTimeline
     .set(carousels, { opacity: 0 })
     .to(carousels, { opacity: 1, delay: 1, stagger: 1, duration: 3 });
 
-const movementTimeline = gsap.timeline({
-    repeat: -1
-});
+carousels.forEach(carousel =>{
+const spanTag = carousel.querySelector("span");
+const spanWidth = spanTag.clientWidth;
 
-movementTimeline
-    .set(carousels, { x:0 })
-    .to(carousels, { x: -400, duration:6, ease: "linear" })
+for (let i = 0; i < 20; i = i + 1) {
+    carousel.appendChild(spanTag.cloneNode(true))
+}
+    const movementTimeline = gsap.timeline({
+        repeat: -1
+    });
+    
+    movementTimeline
+        .set(carousel, { x:0 })
+        .to(carousel, { x: spanWidth * - 1, duration: 6, ease: "linear" })
+
+})
+
--- first/slides.js
+++ first/slides.js
@@ -11,13 +11,32 @@ slides.forEach(slide => {
         z = z - 1;
         image.style.zIndex = z;
     })
-    
+
+    const timeline = gsap.timeline()
+
+    timeline
+    .set(images, {
+        x: () =>{
+            return 500 * Math.random() - 250
+        } , 
+        y:"500%",
+        rotation: () => {
+            return 90 * Math.random() - 45
+        } 
+    })
+    .to(images, { x:0, y:0 , stagger: -0.25})
+    .to(images, { 
+        rotation: () =>{
+            return 16 * Math.random() - 8
+        } 
+    })
+
     slide.addEventListener("click", function(){
         
         z = z - 1;
     
         images[current].style.zIndex = z; 
-        
+
         current = current + 1;
         current = current % images.length;
     })
